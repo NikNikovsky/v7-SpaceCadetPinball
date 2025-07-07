1db839c5ea814bb03036907a5904be0c75e42bc7
oAnblu <194724113+oAnblu@users.noreply.github.com>
2025-07-07 20:25:48 +0300

    "fixed" the closing issue, but it seems to leak memory

--- src/js/process.js
+++ src/js/process.js
@@ -3,6 +3,7 @@ const { RUN } = await load("js/pinball.js");
 
 class proc extends ThirdPartyAppProcess {
   exit;
+  module;
   constructor(handler, pid, parentPid, app, workingDirectory, ...args) {
     super(handler, pid, parentPid, app, workingDirectory);
     this.workingDirectory = workingDirectory;
@@ -12,13 +13,16 @@ class proc extends ThirdPartyAppProcess {
     const body = this.getBody();
     body.innerHTML = html;
 
-    await RUN(body.querySelector("canvas#canvas"), (e) => (this.exit = e));
+    this.module = await RUN(body.querySelector("canvas#canvas"), (e) => (this.exit = e));
   }
 
   async onClose() {
     try {
       await this.exit?.("yes", false);
     } catch {}
+	this.module.pauseMainLoop();
+	this.module.SDL2.audioContext.close();
+	this.module = undefined;
     return true;
   }
 }
